7c7f9fc637c05248d2da4418e467dfce50480b42
vaishnavi paliya <paliyavaishnavi11@gmail.com>
2021-04-16 12:02:10 +0530

    feat: added array of que and ans.

--- index.js
+++ index.js
@@ -11,3 +11,25 @@ console.log('2. You will get 1 points on each Right Answer.');
 console.log('3. One Point will be deducted if the Answer is Wrong.');
 console.log('all the best!!!!')
 console.log('\n');
+var score=0;
+
+var questions=[{
+  question:"who is duryodhan's father ",
+  answer:"drihastara"},
+  {
+    question:"which langauge used in bhagvad gita? ",
+    answer:"sanskrit"
+  },
+  {
+    question:"which guna is associated with passion ? ",
+    answer:"rajas"
+  },
+  {
+    question:"how many verses bhagvad gita has? ",
+    answer:"700"
+  },
+  {
+    question:"lord krishna says he takes avtar whenever ",
+    answer:"less peope follow dharma"
+  }
+  ];
